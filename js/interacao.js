7ea3da266c86a3b4135f6f347f7d8a8e18149f19
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2022-10-28 17:52:42 -0300

    chamamos os textos e os estão sendo dispostos, agr precisamos que a caixa de mensagem comece no fim e proxima etapa é mandar mensagem atualizando a bandeija

--- js/interacao.js
+++ js/interacao.js
@@ -1,3 +1,4 @@
+// =========== login e entrada ao chat ===========
 let nomeDoUsuario = "";
 let nomeParaEnvio = {nome: ""};
 function entrarNaSala(){
@@ -12,7 +13,12 @@ function entrarNaSala(){
 
 function usuarioValido(resposta){
     console.log("usuário valido");
-    setInterval(manterOnline, 5000)
+//    setInterval(manterOnline, 5000)
+
+    const tirarAreaDeLogin = document.querySelector('.secaoDeLogin');
+    tirarAreaDeLogin.classList.add('invisivel');
+
+    carregarMensagensDoServidor()
 }
 function manterOnline(){
     axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nomeParaEnvio);
@@ -20,4 +26,41 @@ function manterOnline(){
 function usuarioInvalido(erro){
     if(erro.response.status === 400){console.log("usuário existente")}
     else {console.log("é outro problema")}
+}
+
+// ======== processamento de mensagem do servidor =========
+
+let mensagens = [];
+function carregarMensagensDoServidor() {
+    
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+
+    promise.then(respostaChegou);
+}
+
+function respostaChegou(resposta) {
+    mensagens = resposta.data;
+
+    paraACaixaDeMensagens()
+}
+
+function paraACaixaDeMensagens() {
+
+    const areaDasMensagens = document.querySelector(".bandeijaDeMensagem");
+    areaDasMensagens.innerHTML = "";
+
+    for (i=0; i<mensagens.length - 1; i++) {
+        let mensagem = mensagens[i];
+
+        areaDasMensagens.innerHTML += `
+        <div class="mensagem flex aling">
+            <span class="spanPrincipal">
+                <span class="horas">(horas)</span>
+                <span class="nomeUsuario">${mensagem.from}</span>
+                <span class="to">para <strong>${mensagem.to}</strong></span>
+                <span class="mensagemUsuario">${mensagem.text}</span>
+            </span>
+        </div>  
+        `;
+    };
 }
